a09c96f3487b1762685f31b870edcb19de2b8ee0
Ingrid <ingrid10arellano@gmail.com>
2018-11-07 08:33:57 -0500

    Preguntas yrespuestasde Ciencia añadidas

---
+++ PreguntasCiencia.py
@@ -0,0 +1,22 @@
+preguntasCiencia = ['1.¿Cuántas patas tienen los cangrejos?','2.¿Cómo se llama el fruto del nogal?','3.El orden de los sumandos no altera el producto, esta afirmación corresponde con:',
+'4.¿Qué animal podría sobrevivir a una explosión nuclear?','5.¿Cómo se llaman las crías del águila?', '6.¿Cómo se llama la parte de circunferencia comprendida entre dos puntos?',
+'7.¿Cuántos grados tiene un ángulo recto?','8.¿Qué propiedad del sonido permite distinguir entre dos notas musicales tocadas por dos instrumentos diferentes?',
+'9.La temperatura a la cual la materia pasa de estado sólido a estado líquido se denomina:','10.¿Cuál es el hueso más largo del ser humano?','11.La ciencia o rama de la biología que estudia los vegetales se denomina:',
+'12.¿Cómo se llama el pigmento verde que le da el color característico a las plantas?','13.La longitud de un punto geográfico es la distancia entre éste y el:','14.La escala de temperatura utilizada en España se llama:',
+'15.¿Cuál es el único número que es par y primo a la vez?','16.¿Qué planeta es famoso por los anillos que lo rodean?','17.¿Qué animales tienen molleja?','18.La cría del jabalí recibe el nombre de:',
+'19.¿Quién descubrió la penicilina?','20.La combinación de dos átomos del mismo elemento da como resultado:','21.¿Cómo es la carga del protón?','22.¿Qué tipo de ganadería se ocupa de las ovejas?',
+'23.El paso del estado de la materia que se encuentra en forma gaseosa a forma líquida se denomina:','24.¿Cuál es la velocidad máxima de la luz en el vacío?','25.¿Cómo se llama al menor número natural que es múltiplo de dos o más números?',
+'26.¿Cuál es el elemento químico cuyo símbolo es Pb?','27.¿Cuál de las siguientes fuentes no es una energía renovable?','28.El intestino delgado tiene una longitud aproximada de:','29.¿Cuál es el primer fruto de la higuera?'
+,'30.Los agujeros de la nariz se llaman:','31.¿Cual es el tiburón que tiene mayor tamaño?'
+,'32.¿Cuántas vueltas da el segundero en una vuelta completa en un reloj de doce horas?','33.Los huesos de la columna vertebral se llaman:'
+,'34.¿Cómo se llama el peso molecular de una sustancia?','35.¿Por qué los camaleones cambian de color?'
+,'36.¿Cuál es la magnitud que relaciona espacio y tiempo?','37.¿Quiénes realizan la nutrición autótrofa?'
+,'38¿Qué sustancia se encuentra en el interior de un termómetro?','39.¿Cuántas patas tiene una abeja?'
+,'40.¿Cómo se denomina a un periodo de 5 años?','41.¿Cuántos kilos hay en una tonelada?'
+,'42.¿Cómo se llama al mayor número que divide dos o más números sin dejar resto?','43.¿Qué líquido segrega el páncreas?'
+,'44.¿Cuál de los siguientes peces es propio de alta mar?']
+
+respuestasCiencia = ["CINCO PARES","NUEZ","LA PROPIEDAD CONMUTATIVA","CUCARACHA","AGUILUCHO","ARCO","NOVENTA","TIMBRE","FUSIÓN","FÉMUR","BOTÁNICA","CLOROFILA","MERIDIANO DE GREENWICH","CENTÍGRADOS","DOS","SATURNO","LAS AVES","JABATO",
+"FLEMMING","MOLÉCULAS","POSITIVA","OVINA","CONDENSACIÓN","TRESCIENTOS MIL KM POR SEGUNDO","MÍNIMO COMÚN MÚLTIPLO","PLOMO","ENERGÍA ATÓMICA","SEIS METROS","BREVA","FOSAS NASALES","EL TIBURÓN BALLENA","SETESCIENTAS VEINTE VUELTAS","VÉRTEBRAS","MOL",
+"POR LOS CROMATÓFOROS","VELOCIDAD","LAS PLANTAS","MERCURIO","SEIS","QUINQUENIO","MIL KILOGRAMOS","MÁXIMO COMÚN DIVISOR","LA INSULINA","SARDINA"]
+
